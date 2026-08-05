4c93eefe281165896be7cb0b7628d2675cd7c662
ZQ-Struggle <zzzzzq1998@gmail.com>
2026-08-05 22:19:05 +0800

    build: use official FlagPrism main branch

--- python/setup_tools/utils/__init__.py
+++ python/setup_tools/utils/__init__.py
@@ -13,8 +13,7 @@ flagtree_submodules = {
     tools.Module(name="flir", url="https://github.com/flagos-ai/flir.git", commit_id="",
                  dst_path=os.path.join(flagtree_configs.flagtree_submodule_dir, "flir")),
     "flagprism":
-    tools.Module(name="FlagPrism", url="https://github.com/ZQ-Struggle/FlagTree_DevTools.git",
-                 commit_id="1b93d3143745e4d06a6726d570e3313000eb4156",
+    tools.Module(name="FlagPrism", url="https://github.com/flagos-ai/FlagPrism.git", branch="main",
                  dst_path=os.path.join(flagtree_configs.flagtree_submodule_dir, "FlagPrism")),
 }
 
